52d39927970b1b726f37d3000ce36d2cec9c22bd
Omar Gates <gatesoc@gmail.com>
2026-01-21 21:40:18 -0500

    Adds Streamlit Secrets to the app

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -12,15 +12,42 @@ st.write('''This app uses 6 inputs to predict the species of penguin
          using a model built on the Palmer Penguins dataset. Use
          the form below to get started!''')
 
+password_guess = st.text_input('What is the Password?')
+if password_guess != st.secrets['password']:
+    st.stop()
 
-penguin_df = pd.read_csv('penguins.csv')
+penguin_file = st.file_uploader('Upload your own penguin data')
 
-rf_pickle = open('random_forest_penguin.pickle', 'rb')
-map_pickle = open('output_penguin.pickle', 'rb')
-rfc = pickle.load(rf_pickle)
-unique_penguin_mapping = pickle.load(map_pickle)
-rf_pickle.close()
-map_pickle.close()
+if penguin_file is None:
+    rf_pickle = open('random_forest_penguin.pickle', 'rb')
+    map_pickle = open('output_penguin.pickle', 'rb')
+    rfc = pickle.load(rf_pickle)
+    unique_penguin_mapping = pickle.load(map_pickle)
+    rf_pickle.close()
+    map_pickle.close()
+else:
+    penguin_df = pd.read_csv(penguin_file)
+    penguin_df = penguin_df.dropna()
+    output = penguin_df['species']
+    features = penguin_df[['island',
+                           'bill_length_mm',
+                           'bill_depth_mm',
+                           'flipper_length_mm',
+                           'body_mass_g',
+                           'sex']]
+    features = pd.get_dummies(features)
+    output, unique_penguin_mapping = pd.factorize(output)
+    x_train, x_test, y_train, y_test = train_test_split(features,
+                                                        output,
+                                                        test_size= 0.8)
+    rfc = RandomForestClassifier(random_state = 15)
+    rfc.fit(x_train.values, y_train)
+    y_pred = rfc.predict(x_test.values)
+    score = round(accuracy_score(y_pred, y_test), 2)
+    st.write(f'''We trained a Random Forest Model on these data,
+             it has a score of {score}! Use the inputs below to
+             try out the model''')
+
 
 with st.form('user_inputs'):
     island = st.selectbox('Penguin Island',
